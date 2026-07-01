0b8d457eea8fbe78df2e909172177011252b6714
Ionut Grecu <ionut@grecu.eu>
2026-07-01 09:33:36 +0300

    Add property docblock to Neighborhood model

--- src/Models/Neighborhood.php
+++ src/Models/Neighborhood.php
@@ -5,6 +5,20 @@ namespace Ionutgrecu\LaravelGeo\Models;
 use Illuminate\Database\Eloquent\Model;
 use function config;
 
+/**
+ * Class Neighborhood
+ * @package Ionutgrecu\LaravelGeo\Models
+ * @property int id
+ * @property string city_code
+ * @property string code
+ * @property string name
+ * @property string wiki_data_id
+ * @property string latitude
+ * @property string longitude
+ * @property string polygon
+ * @property string created_at
+ * @property string updated_at
+ */
 class Neighborhood extends Model {
     protected $guarded = ['id', 'created_at'];
 
